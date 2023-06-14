c80650a7a56afc1fcc8a795b516bd5340a0c90d8
LaraBatista04 <100876601+LaraBatista04@users.noreply.github.com>
2023-06-14 10:52:49 -0300

    first commit

---
+++ no.c
@@ -0,0 +1,11 @@
+#include "no.h"
+struct no * aloca_no (int i){
+    struct no * novo = (struct no *) malloc(sizeof(struct no));
+    if (novo != 'NULL'){
+        novo->info = i;
+        novo->proximo = 'NULL';
+        novo->primeiro = 'NULL';
+        novo->ultimo = 'NULL';
+    }
+    return novo;
+}
